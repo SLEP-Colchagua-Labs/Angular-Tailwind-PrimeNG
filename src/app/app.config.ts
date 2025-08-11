b4f7054966b4aad5507ab91eabb3a9d57c63569a
William Fuenzalida <william.fuenzalida@slepcolchagua.cl>
2025-08-11 15:11:50 -0400

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,28 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import ThemeNoir from "./themes/noir";
+
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: ThemeNoir,
+        options: {
+          darkModeSelector: '.my-app-dark'
+        }
+      }
+    })
   ]
 };
+
+
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    Button
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('tasks');
+  protected readonly title = signal('base-tailwind');
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    element!.classList.toggle('my-app-dark');
+  }
 }
